d53ff10dccfaa0b6a18bf3c2665041fdc836faa4
amigodejuanes-cpu <amigodejuanes@gmail.com>
2026-09-05 14:09:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const mineflayer = require('mineflayer');
 
 function createBot() {
     const bot = mineflayer.createBot({
-        host: 'Idontkow.aternos.me', // 1. CAMBIA ESTO por la IP de tu servidor
-        port: 25565,
+        host: 'zebrafish.aternos.host', // 1. CAMBIA ESTO por la IP de tu servidor
+        port: 33753,
         username: 'Bot',          // El nombre que tendrá tu bot
         version: '1.21.1'                      // 2. CAMBIA ESTO por tu versión si es otra
     });
